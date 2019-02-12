8fca579bfa76ad38866bf74e71ef17d6e157be90
Javier Aceña <40375118+j0nl1@users.noreply.github.com>
2019-02-12 15:17:14 +0100

    Menu Depending If Logged (#3)
    * Menu Completed
    
    * Menu Depending If Logged

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,9 @@
+$(document).ready(function () {
+  $('#sidebarCollapse').on('click', function () {
+    $('#sidebar').toggleClass('active');
+  });
+});
+
 document.addEventListener('DOMContentLoaded', () => {
 
   console.log('IronGenerator JS imported successfully!');
