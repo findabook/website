a84c6d055ef6ca48cdb53f37ad665e68d84e0ac3
j0nl1 <jonlitech@gmail.com>
2019-02-13 13:30:28 +0100

    Fix userSchema

--- models/User.js
+++ models/User.js
@@ -5,7 +5,13 @@ const userSchema = new Schema(
 	{
 		username: { type: String, unique: true },
 		password: String,
-		location: String,
+		location: {
+			type: String,
+			coords: {
+				lat: Number,
+				lng: Number
+			}
+		},
 		preferences: [{ type: String, enum: ["thriller", "novel", "terror", "police", "children", "romantic", "adventures", "politics", "comedy"]}],
 		chats: [{ type: Schema.Types.ObjectId, ref: "Chat" }]
 	},
