a463a05ce4a9dd2e6ea27ae88a38e9256ea5c1f2
Jorge <45262623+JorgeLegazpe@users.noreply.github.com>
2019-02-13 19:11:15 +0100

    Problemas con maps (#7)
    * map create and views books and help
    
    * Problemas con maps

---
+++ public/javascripts/maps.js
@@ -0,0 +1,30 @@
+let map;
+
+function startMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 5,
+    center: {
+      lat: 40.222,
+      lng: 3.22
+    }
+  });
+}
+
+function showMarkers() {
+  axios.get(`/map`).then(book => {
+    console.log(book);
+    book.data.actualOwners.forEach(user => {
+      new google.maps.Marker({
+        map: map,
+        position: {
+          lat: user.location.coords.lat,
+          lng: user.location.coords.lng
+        },
+        title: user.username
+      });
+    });
+  });
+}
+
+startMap();
+showMarkers();
--- routes/books.js
+++ routes/books.js
@@ -1,13 +1,20 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const books = require("../models/Book");
 
-router.get('/used', (req, res, next) => {
-    res.render('books/used')
-})
+router.get("/used", (req, res, next) => {
+  res.render("books/used");
+});
 
-router.get('/new', (req, res, next) => {
-    res.render('books/new')
-})
+router.get("/new", (req, res, next) => {
+  res.render("books/new");
+});
+
+
+
+router.get("/trending", (req, res, next) => {
+  res.render("books/trending");
+});
 
 router.get('/add', (req, res, next) => {
     res.render('books/add')
@@ -17,13 +24,16 @@ router.get('/trending', (req, res, next) => {
     res.render('books/trending')
 })
 
+
 router.get("/used/:name", (req, res, next) => {
-    res.render("books/singleview")
-})
+  res.render("books/singleview");
+});
 
+router.get("/map/:id", (req, res, next) => {
+  books.find({ _id: req.params.id }).then(book => {
+    res.json(book);
+    console.log(book);
+  });
+});
 
-
-
-
-
-module.exports = router
+module.exports = router;
