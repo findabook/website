bbb08cb12e2815cf474d77593ab51905ecca7ff6
Javier Aceña <40375118+j0nl1@users.noreply.github.com>
2019-02-14 13:39:27 +0100

    Info from API (#9)

--- routes/books.js
+++ routes/books.js
@@ -2,30 +2,53 @@ const express = require("express");
 const router = express.Router();
 const books = require("../models/Book");
 
-router.get("/used", (req, res, next) => {
+router.get("/used", (req, res) => {
   res.render("books/used");
 });
 
-router.get("/new", (req, res, next) => {
+router.get("/new", (req, res) => {
   res.render("books/new");
 });
 
 
 
-router.get("/trending", (req, res, next) => {
+router.get("/trending", (req, res) => {
   res.render("books/trending");
 });
 
-router.get('/add', (req, res, next) => {
+router.get('/add', (req, res) => {
     res.render('books/add')
 })
 
-router.get('/trending', (req, res, next) => {
+router.post('/add', (req, res) => {
+  let categories = req.body.categories.split(" / ")
+  categories = categories.filter((categorie, i) => categories.indexOf(categorie) === i)
+  books.findOne({ name: req.body.name })
+    .then(book => {
+      if (book == null) {
+        books.create({
+          name: req.body.name,
+          author: req.body.author,
+          resume: req.body.resume,
+          categories,
+          imageUrl: req.body.image,
+          quantity: 1,
+          actualOwners: [req.user._id],
+        })
+        res.redirect("/books/used/" + req.body.name)
+        return
+      }
+      books.findOneAndUpdate({ name: req.body.name }, { $inc: {quantity: 1}, $push: {actualOwners: req.user._id} },{new: true})
+        .then(() => res.redirect("/books/used/" + req.body.name))
+  })
+})
+
+router.get('/trending', (req, res) => {
     res.render('books/trending')
 })
 
 
-router.get("/used/:name", (req, res, next) => {
+router.get("/used/:name", (req, res) => {
   books.findOne({ name: req.params.name })
     .populate("users")
     .then(book => {
